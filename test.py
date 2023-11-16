9e980d5d34ac6ba225144401051086988067bf5a
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2023-11-16 12:13:03 +0100

    forbind med azure

--- test.py
+++ test.py
@@ -1,33 +1,38 @@
 #%%
+import os
 import openai
 import getpass
 #%%
 openai.api_key = getpass.getpass()
 
+#%%
 openai.api_base =  'https://faggruppe-gpt.openai.azure.com/' 
 openai.api_type = 'azure' # Necessary for using the OpenAI library with Azure OpenAI
 openai.api_version = '2023-08-01-preview' # Latest / target version of the API
 
 deployment_name = 'gpt-4' # SDK calls this "engine", but naming
                                            # it "deployment_name" for clarity
-
 #%%
-prompt = 'What is Azure OpenAI?'
-response = openai.Completion.create(engine=deployment_name, prompt=prompt)
-
-print(response.choices[0].text)
+response = openai.ChatCompletion.create(
+    engine="gpt-4", # The deployment name you chose when you deployed the GPT-3.5-Turbo or GPT-4 model.
+    messages=[
+        {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+        {"role": "user", "content": "Tell a joke"}
+    ]
+)
 #%%
-
+print(response['choices'][0]['message']['content'])
+#%%
 def ask_gpt(query):
 
    
     gpt_instructions = """System: Follow these three instructions: 
         System: 1. Tell a joke,
-        System: 3. Stick to Norwegian.   
+        System: 3. Stick to New Norwegian.   
         """
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
+        engine="gpt-4",
         messages=[
             {"role": "system", "content": gpt_instructions},
             {"role": "assistant", "content": ""},
